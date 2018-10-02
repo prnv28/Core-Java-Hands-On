db22de535adc808a70a95a0a71473f702cfaef38
PRANAV GAJERA <pranavgajera1998@gmail.com>
2018-10-02 17:39:15 +0530

    Fifth Progress (Addition of Thread class and Runnable interface)

---
+++ src/CurrentThreadDemo.java
@@ -0,0 +1,20 @@
+// Controlling the main Thread.
+public class CurrentThreadDemo {
+    public static void main(String args[]){
+        Thread t=Thread.currentThread();
+        System.out.println("Current Thread : "+t);
+
+        // change the name of the thread
+        t.setName("My Thread");
+        System.out.println("After name change : "+t);
+        try{
+            for(int n=5;n>0;n--){
+                System.out.println(n);
+                Thread.sleep(2000);
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Main Thread Interrupted");
+        }
+    }
+}
